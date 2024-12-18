ae24d5ef67151f3f756b79862cdcd7bdd63dcb69
amircodel <amirali.mohammadi890@gmail.com>
2024-12-18 18:49:08 +0330

    update bubble_sort.asm

--- bubble_sort.asm
+++ bubble_sort.asm
@@ -1,7 +1,11 @@
-org 100h 
+org 100h
+
+.model small
+.stack 100h
+ 
 .data
 
-array  db 1,4,2,3 
+array  db 1,40,2,3 
 length  dw 4
 
 .code   
@@ -20,8 +24,7 @@ cmponarray:
     jnc noswap           
     mov array[si],dl
     mov array[si+1],al
-
-
+    
 noswap:
     inc si   
 
@@ -30,19 +33,52 @@ noswap:
 
     loop iteronarray
 
-    mov cx,length
-    mov si,0
+    mov di,length
+    mov si,0   
 
+
+;; notice: this code is for resualt output/printing purposes only and is not part of the main program or algorithm
 print:
-    mov al, array[si]
-    add al, 30h
-    mov ah, 0eh
-    int 10h
+    mov ax, @data
+    mov ds, ax
+    
+    mov dh, 0      
+    mov dl, array[si]
+    mov ax, dx
 
-    mov al, ' '
-    int 10h
+    mov cx, 0
+    mov dx, 0
 
-    inc si
-    loop print
+count_digit:
+    cmp ax, 0
+    je print_digit
+
+    mov bx, 10
+    div bx
+    push dx
+    inc cx
+    xor dx, dx
+    jmp count_digit
+
+print_digit:
+    cmp cx, 0
+    je exit
+
+    pop dx
+    add dl, 48
+    mov ah, 02H    
+    int 21H           
+    dec cx              
+    jmp print_digit  
+
+exit:
+    mov ah, 02H  
+    mov dl, ' '     
+    int 21H
+
+    inc si             
+    dec di
+    
+    jnz print       
 
     ret
